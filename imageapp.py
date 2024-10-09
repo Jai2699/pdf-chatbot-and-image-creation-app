91739aaa62dded120109e791809a09151994f5b2
Jai Rajkumar Mane <67153514+Jai2699@users.noreply.github.com>
2024-10-09 18:16:33 +0530

    Add files via upload

--- imageapp.py
+++ imageapp.py
@@ -1,36 +1,36 @@
-from openai import OpenAI
+from openai import OpenAI  # openai 1.51.0
 from PIL import Image
 import streamlit as st
 from apikey import apikey
-
-#Initialize image generation client
-client= OpenAI(api_key=apikey)
+#Initialize your image generation client
+client=OpenAI(api_key=apikey)
 
 def generate_images(image_description, num_images):
-    images=[]
-    img_response= client.images.generate(
-        model="dall-e-3",
-        prompt= image_description,
-        size='1024x1024',
-        quality= 'standard',
-        n=1
+    images=[] #
+    for i in range(num_images):#
         
-    )
-    image_url=img_response.data[0].url
-    return image_url
+        img_response=client.images.generate(
+            model="dall-e-3",
+            prompt=image_description,
+            size='1024x1024',
+            quality='standard',
+            n=1
+        )
+        image_url= img_response.data[0].url#
+        images.append(image_url)#
+    return images#
+st.set_page_config(page_title="Image-Generation", page_icon=":camera:",layout="wide")
+#create a title
+# st.title("Image-Generation")
+st.markdown("<h1 style='text-align: center;'>Image-Generation</h1>", unsafe_allow_html=True)
+#Create a subheader
+st.subheader("Powered by the most powerful image generation APP")
+img_description=st.text_input("Enter a description for the image")
+num_of_images=st.number_input("Select the number of images you want to generate",min_value=1, max_value=5, value=1)
 
-st.set_page_config(page_title="Image-Generation", page_icon=":camera:", layout="wide")
-
-#Image generation title
-st.title("Image-Generation")
-
-#Create subheader
-st.subheader("Powered by most Powerful image generation APP")
-
-img_description= st.text_input("Enter a description for the image")
-num_of_images= st.number_input("Select the number of images you want to generate", min_value=1, max_value=5, value=1)
-
-#Create button
-if st.button("Generate image"):
-    generate_image= generate_images(img_description, num_of_images)
-    st.image(generate_image)
+#create button
+if st.button("Generate Images"):
+    generate_image=generate_images(img_description, num_of_images)#
+    
+    for i in range(len(generate_image)):#
+        st.image(generate_image[i])#
